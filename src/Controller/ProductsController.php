b36b1310be9977640c479ee32e3c102fdefac8be
Maxim Usenko <fightstyle81@gmail.com>
2019-11-26 22:21:48 +0200

    Created show categories

--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Service\Category\CategoryServiceInterface;
 use App\Service\Product\ProductPresentationService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,11 +14,13 @@ class ProductsController extends AbstractController
      * @Route("/products", name="products")
      *
      * @param ProductPresentationService $presentationService
+     * @param CategoryServiceInterface $categoryService
      * @return Response
      */
-    public function show(ProductPresentationService $presentationService): Response
+    public function show(ProductPresentationService $presentationService, CategoryServiceInterface $categoryService): Response
     {
         return $this->render('products/products.html.twig', [
+            'categories' => $categoryService->getCategories(),
             'products' => $presentationService->getPublished(),
         ]);
     }
