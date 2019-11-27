909ae4e4ad8d817dd17c6d95f506aaac78b7269f
Maxim Usenko <fightstyle81@gmail.com>
2019-11-27 20:37:55 +0200

    Added send to mail with swift mailer

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Form\ContactType;
 use App\Service\Contact\ContactServiceInterface;
+use Swift_Mailer;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -15,16 +16,36 @@ class ContactController extends AbstractController
      * @Route("/contact", name="contact")
      * @param Request $request
      * @param ContactServiceInterface $contactService
+     * @param Swift_Mailer $mailer
      * @return Response
      */
-    public function show(Request $request, ContactServiceInterface $contactService): Response
+    public function show(Request $request, ContactServiceInterface $contactService, \Swift_Mailer $mailer): Response
     {
         $form = $this->createForm(ContactType::class);
 
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $contactService->insertContactData($form->getData());
+            $data = $form->getData();
+
+            $message = (new \Swift_Message('Hello Email'))
+                ->setFrom('send@example.com')
+                ->setTo('recipient@example.com')
+                ->setBody(
+                    $this->renderView('email/contact_email.html.twig',
+                        [
+                            'name' => $data['name'],
+                            'email' => $data['email'],
+                            'subject' => $data['subject'],
+                            'message' => $data['message'],
+                        ]
+                    ),
+                    'text/html'
+                );
+
+            $mailer->send($message);
+
+            $contactService->insertContactData($data);
 
             return $this->render('contact/success.html.twig', [
             ]);
