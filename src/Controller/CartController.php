49936d79d7e9948fe2aee8b35dfb66a8c4bb1ac1
Maxim Usenko <fightstyle81@gmail.com>
2019-11-27 17:28:20 +0200

    Created routes for carts

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -2,18 +2,44 @@
 
 namespace App\Controller;
 
+use App\Service\Cart\CartService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CartController extends AbstractController
 {
     /**
+     * @var CartService
+     */
+    protected $cartService;
+
+    /**
+     * CartController constructor.
+     * @param CartService $cartService
+     */
+    public function __construct(CartService $cartService)
+    {
+        $this->cartService = $cartService;
+    }
+
+    /**
      * @Route("/cart", name="cart")
      */
-    public function index()
+    public function showCart(): Response
     {
         return $this->render('cart/cart.html.twig', [
-            'controller_name' => 'CartController',
+        ]);
+    }
+
+    /**
+     * @return Response
+     *
+     */
+    public function miniCart(): Response
+    {
+        return $this->render('cart/minicart.html.twig', [
+            'countProducts' => $this->cartService->countProducts(),
         ]);
     }
 }
