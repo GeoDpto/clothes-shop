116607d1827010837b7727a6d14ca65cb5b62889
Maxim Usenko <fightstyle81@gmail.com>
2019-11-27 19:26:43 +0200

    Created contact form

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Form\ContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,7 +13,10 @@ class ContactController extends AbstractController
      */
     public function show()
     {
+        $form = $this->createForm(ContactType::class);
+
         return $this->render('contact/contact.html.twig', [
+            'contact_form' => $form->createView(),
         ]);
     }
 }
