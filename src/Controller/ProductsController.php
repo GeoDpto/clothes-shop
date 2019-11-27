4f735712242f699264a06cd93188ed048a5f6764
Maxim Usenko <fightstyle81@gmail.com>
2019-11-27 14:45:56 +0200

    Created pagination for products

--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -4,7 +4,9 @@ namespace App\Controller;
 
 use App\Service\Category\CategoryServiceInterface;
 use App\Service\Product\ProductPresentationService;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,16 +14,19 @@ class ProductsController extends AbstractController
 {
     /**
      * @Route("/products", name="products")
-     *
      * @param ProductPresentationService $presentationService
      * @param CategoryServiceInterface $categoryService
+     * @param PaginatorInterface $paginator
+     * @param Request $request
      * @return Response
      */
-    public function show(ProductPresentationService $presentationService, CategoryServiceInterface $categoryService): Response
+    public function show(ProductPresentationService $presentationService, CategoryServiceInterface $categoryService, PaginatorInterface $paginator, Request $request): Response
     {
+        $products = $presentationService->getPaginatedProducts($request->query->getInt('page', 1));
+
         return $this->render('products/products.html.twig', [
             'categories' => $categoryService->getCategories(),
-            'products' => $presentationService->getPublished(),
+            'products' => $products,
         ]);
     }
 }
