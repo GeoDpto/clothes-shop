e1182302583c5fba28c86e27239f6bddeeba1f8d
Maxim Usenko <fightstyle81@gmail.com>
2019-11-28 14:18:09 +0200

    Added view products in checkout

--- src/Controller/CheckoutController.php
+++ src/Controller/CheckoutController.php
@@ -2,18 +2,23 @@
 
 namespace App\Controller;
 
+use App\Service\Cart\CartService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CheckoutController extends AbstractController
 {
     /**
      * @Route("/checkout", name="checkout")
+     * @param CartService $cartService
+     * @return Response
      */
-    public function checkout()
+    public function checkout(CartService $cartService): Response
     {
         return $this->render('checkout/checkout.html.twig', [
-            'controller_name' => 'CheckoutController',
+            'totalPrice' => $cartService->getTotalPrice(),
+            'products' => $cartService->getCartProducts(),
         ]);
     }
 }
